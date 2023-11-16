22f44d3845fd03565809aa3a082df711520152a9
alope107 <auberonlopez@gmail.com>
2023-11-16 13:50:37 -0800

    Mock recaptcha and axios.
    - Add mock objects for recaptcha and axios in tests.

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,29 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import axios from "axios";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders without crashing', () => {
+const mockExecuteRecaptcha = jest.fn();
+
+// Adapted from: https://github.com/t49tran/react-google-recaptcha-v3/issues/123
+jest.mock("react-google-recaptcha-v3", () => {
+  return {
+    GoogleReCaptchaProvider: ({ children }) => {
+      return <>{children}</>;
+    },
+    useGoogleReCaptcha: () => ({
+      executeRecaptcha: mockExecuteRecaptcha,
+    }),
+  };
+});
+
+jest.mock("axios");
+const axiosGet = axios.get;
+
+beforeEach(() => {
+  axiosGet.mockReset();
+  mockExecuteRecaptcha.mockReset();
+});
+
+test("renders without crashing", () => {
   render(<App />);
 });
