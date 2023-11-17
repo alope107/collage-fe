b2f99bc4bfad6e7e49d54495b763c26b79850657
alope107 <auberonlopez@gmail.com>
2023-11-16 17:41:16 -0800

    Add test for submitting form.
    - Tests that form properly accepts input and sends it to the right URL.

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import App from "./App";
 
 const mockExecuteRecaptcha = jest.fn();
@@ -18,12 +18,59 @@ jest.mock("react-google-recaptcha-v3", () => {
 
 jest.mock("axios");
 const axiosGet = axios.get;
+const axiosPost = axios.post;
 
 beforeEach(() => {
   axiosGet.mockReset();
+  axiosPost.mockReset();
   mockExecuteRecaptcha.mockReset();
 });
 
 test("renders without crashing", () => {
   render(<App />);
+});
+
+test("form is submitted properly", async () => {
+  mockExecuteRecaptcha.mockResolvedValue("mockToken");
+
+  const { getByLabelText, getByText } = render(<App />);
+
+  const fastaInput = getByLabelText("FASTA");
+  const fastaFile = new File(["mockFastaData"], "fasta.txt", {
+    type: "text/plain",
+  });
+  fireEvent.change(fastaInput, { target: { files: [fastaFile] } });
+
+  const speciesTextBox = getByLabelText("Species");
+  fireEvent.change(speciesTextBox, { target: { value: "mockSpecies" } });
+
+  const submitButton = getByText("Submit");
+  fireEvent.click(submitButton);
+
+  axiosPost.mockResolvedValue({
+    status: 200,
+    data: {
+      is_valid: true,
+      id: "mockId",
+    },
+  });
+
+  await waitFor(() => {
+    expect(axiosPost).toHaveBeenCalledTimes(1);
+    const [actualUrl, actualFormData] = axiosPost.mock.calls[0];
+    expect(actualUrl).toEqual("https://mock-job-request.url/request/");
+
+    const expectedFormObject = {
+      fasta: fastaFile,
+      token: "mockToken",
+      species: "mockSpecies",
+    };
+
+    const actualFormObject = Object.fromEntries(actualFormData.entries());
+
+    // TODO(auberon):
+    // toEqual does not properly check the contents of the file uploaded
+    // consider moving to another method that actually checks the file data
+    expect(actualFormObject).toEqual(expectedFormObject);
+  });
 });
