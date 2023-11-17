1e5c706097adce952e80fa17d89c7a0dd5bed853
alope107 <auberonlopez@gmail.com>
2023-11-17 14:53:59 -0800

    Add useInterval custom hook.
    - Adds a hook that will be used for polling.

---
+++ src/hooks/useInterval.js
@@ -0,0 +1,25 @@
+import { useEffect, useRef } from "react";
+
+// Taken from https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+// Used to make sure latest props are used when using an interval
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
+
+export default useInterval;
