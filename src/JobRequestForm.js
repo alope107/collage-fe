ff5284848d2dc2c1706fca77dcec415064a05c67
alope107 <auberonlopez@gmail.com>
2024-01-02 16:34:17 -0800

    Container and submit button styling.
    - Add container / row / col to center input form.
    
    - Make submit button greyed out when disabled.

--- src/JobRequestForm.js
+++ src/JobRequestForm.js
@@ -2,6 +2,9 @@ import PropTypes from "prop-types";
 import { useRef, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 const INITIAL_FORM_DATA = {
   fasta: "",
@@ -10,6 +13,8 @@ const INITIAL_FORM_DATA = {
 
 const JobRequestForm = ({ jobRequestCallback, canSubmit, speciesList }) => {
   const [requestData, setRequestData] = useState(INITIAL_FORM_DATA);
+
+  canSubmit = canSubmit && requestData.fasta && requestData.species;
 
   const fastaUploadRef = useRef();
 
@@ -27,41 +32,47 @@ const JobRequestForm = ({ jobRequestCallback, canSubmit, speciesList }) => {
   ));
 
   return (
-    <Form onSubmit={submitJobRequest}>
-      <Form.Group controlId="fasta" className="mb-3">
-        <Form.Label>FASTA</Form.Label>
-        <Form.Control
-          type="file"
-          ref={fastaUploadRef}
-          onChange={(e) => {
-            setRequestData({
-              ...requestData,
-              fasta: e.target.files[0],
-            });
-          }}
-        />
-      </Form.Group>
+    <Container fluid className="d-flex h-100">
+      <Row className="justify-content-center align-self-center w-100">
+        <Col xs={12} md={6} lg={4} className="text-center">
+          <Form onSubmit={submitJobRequest}>
+            <Form.Group controlId="fasta" className="mb-3">
+              <Form.Label>FASTA</Form.Label>
+              <Form.Control
+                type="file"
+                ref={fastaUploadRef}
+                onChange={(e) => {
+                  setRequestData({
+                    ...requestData,
+                    fasta: e.target.files[0],
+                  });
+                }}
+              />
+            </Form.Group>
 
-      <Form.Group controlId="species">
-        <Form.Label>Species</Form.Label>
-        <Form.Select
-          value={requestData.species}
-          onChange={(e) => {
-            setRequestData({ ...requestData, species: e.target.value });
-          }}
-        >
-          {speciesOptions}
-        </Form.Select>
-      </Form.Group>
+            <Form.Group controlId="species">
+              <Form.Label>Species</Form.Label>
+              <Form.Select
+                value={requestData.species}
+                onChange={(e) => {
+                  setRequestData({ ...requestData, species: e.target.value });
+                }}
+              >
+                {speciesOptions}
+              </Form.Select>
+            </Form.Group>
 
-      <Button
-        type="submit"
-        variant={canSubmit ? "primary" : "secondary"}
-        disabled={!canSubmit}
-      >
-        Submit
-      </Button>
-    </Form>
+            <Button
+              type="submit"
+              variant={canSubmit ? "primary" : "secondary"}
+              disabled={!canSubmit}
+            >
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
